16c2c841cf747fb42bf7a94af8b13a4b351b4b65
SuJin Park <psjin230@gmail.com>
2022-01-08 12:09:07 +0900

    first-folder

---
+++ Chapter01/src/sec03/exam01/Hello.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+/*
+ 작성자: p
+ 작성일: 2022.01.04
+ */
+
+public class Hello {
+	//프로그램실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");	
+	}//end of main
+}//end of class
