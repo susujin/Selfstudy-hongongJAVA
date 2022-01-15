eaf55f874b766616b0a8f468e58a441e39e20fbe
SuJin Park <psjin230@gmail.com>
2022-01-15 19:08:32 +0900

    연습문제5

---
+++ 연습문제/src/chapter05/Sec02No05.java
@@ -0,0 +1,29 @@
+package chapter05;
+
+public class Sec02No05 {
+
+	public static void main(String[] args) {
+		int[][] array= {
+				{95,86},
+				{83,92,96},
+				{78,83,93,87,88}
+		};
+		
+		int sum=0;
+		double avg=0.0;
+		
+		int avg_count=0;
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array[i].length;j++) {
+				sum+=array[i][j];
+				avg_count++;
+			}
+		}
+		avg=(double)sum/avg_count;
+		
+		System.out.println("sum : "+sum);
+		System.out.println("avg : "+avg);
+
+	}
+
+}
