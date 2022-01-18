d198e9dfe052bd9eca4120db8da4b67464558d07
SuJin Park <psjin230@gmail.com>
2022-01-18 18:13:34 +0900

    연습문제6

---
+++ 연습문제/src/chapter06/Member.java
@@ -0,0 +1,8 @@
+package chapter06;
+
+public class Member {
+	String name;
+	String id;
+	String password;
+	int age;
+}
---
+++ 연습문제/src/chapter06/MemberEx.java
@@ -0,0 +1,12 @@
+package chapter06;
+
+public class MemberEx {
+
+	public static void main(String[] args) {
+		Member member=new Member();
+		member.name="ÃÖÇÏ¾á";
+		member.age=23;
+
+	}
+
+}
