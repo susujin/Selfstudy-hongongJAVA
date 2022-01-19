afa3297b6bd4f7b46f3598920c7721420263a907
SuJin Park <psjin230@gmail.com>
2022-01-19 17:41:06 +0900

    06-2

---
+++ Chapter06/src/sec03/ex01/Car.java
@@ -0,0 +1,7 @@
+package sec03.ex01;
+
+public class Car {
+	Car(String color,int cc){
+		
+	}
+}
---
+++ Chapter06/src/sec03/ex01/CarEx.java
@@ -0,0 +1,10 @@
+package sec03.ex01;
+
+public class CarEx {
+
+	public static void main(String[] args) {
+		Car myCar=new Car("∞À¡§",3000);
+
+	}
+
+}
