a2fbec99a4276af3b9f33f53c0745059360107b5
SuJin Park <psjin230@gmail.com>
2022-02-05 21:16:07 +0900

    07-9

---
+++ Chapter07/src/sec03/ex01/Phone.java
@@ -0,0 +1,17 @@
+package sec03.ex01;
+
+public class Phone {
+	public String owner;
+	
+	public Phone(String owner) {
+		this.owner=owner;
+	}
+	
+	public void turnOn() {
+		System.out.println("폰 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("폰 전원을 끕니다.");
+	}
+}
---
+++ Chapter07/src/sec03/ex01/PhoneEx.java
@@ -0,0 +1,14 @@
+package sec03.ex01;
+
+public class PhoneEx {
+
+	public static void main(String[] args) {
+		//Phone phone=new Phone();
+		SmartPhone smartPhone=new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+
+}
