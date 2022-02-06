82d0f8fd487afa8177c7aeefe54e5a0eca92d423
SuJin Park <psjin230@gmail.com>
2022-02-06 14:29:03 +0900

    연습문제7-3

---
+++ 연습문제/src/Chapter07/HttpServlet.java
@@ -0,0 +1,5 @@
+package Chapter07;
+
+public abstract class HttpServlet {
+	public abstract void service();
+}
---
+++ 연습문제/src/Chapter07/HttpServletEx.java
@@ -0,0 +1,15 @@
+package Chapter07;
+
+public class HttpServletEx {
+
+	public static void main(String[] args) {
+		method(new LoginServlet());
+		method(new FileDownloadServlet());
+
+	}
+
+	public static void method(HttpServlet servlet) {
+		servlet.service();
+	}
+
+}
