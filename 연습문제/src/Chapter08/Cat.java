edfc973768c2c9dfa120913d8a087097512d0ddb
SuJin Park <psjin230@gmail.com>
2022-02-08 20:15:20 +0900

    연습문제8-1

---
+++ 연습문제/src/Chapter08/Cat.java
@@ -0,0 +1,9 @@
+package Chapter08;
+
+public class Cat implements Soundable{
+	@Override
+	public String sound() {
+		//System.out.println("具克");
+		return "具克";
+	}
+}
---
+++ 연습문제/src/Chapter08/Dog.java
@@ -0,0 +1,9 @@
+package Chapter08;
+
+public class Dog implements Soundable{
+	@Override
+	public String sound() {
+		//System.out.println("港港"); 捞霸 酒聪匙..
+		return "港港";
+	}
+}
