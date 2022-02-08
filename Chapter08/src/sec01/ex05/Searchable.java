e2774ed5703e60464e534ac6d81cbee6191fff38
SuJin Park <psjin230@gmail.com>
2022-02-08 16:20:40 +0900

    08-2

---
+++ Chapter08/src/sec01/ex05/Searchable.java
@@ -0,0 +1,5 @@
+package sec01.ex05;
+
+public interface Searchable {
+	void search(String url);
+}
---
+++ Chapter08/src/sec01/ex05/SmartTelevisionEx.java
@@ -0,0 +1,12 @@
+package sec01.ex05;
+
+public class SmartTelevisionEx {
+
+	public static void main(String[] args) {
+		SmartTelevision tv=new SmartTelevision();
+		
+		RemoteControl rc=tv;
+		Searchable searchable=tv;
+	}
+
+}
