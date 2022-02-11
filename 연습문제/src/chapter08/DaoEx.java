318a359de06d51f42a7e586e33c5400976246385
SuJin Park <psjin230@gmail.com>
2022-02-11 19:59:52 +0900

    연습문제8

---
+++ 연습문제/src/chapter08/DaoEx.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+public class DaoEx {
+
+	public static void dbWork(DataAccessObject dao) {
+		dao.select();
+		dao.insert();
+		dao.update();
+		dao.delete();
+	}
+	public static void main(String[] args) {
+		dbWork(new OracleDao());
+		dbWork(new MySqlDao());
+
+	}
+
+}
