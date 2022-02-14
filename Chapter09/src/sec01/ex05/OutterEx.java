cf50eee72151bbc48b4b391240cf9ec4e052057b
SuJin Park <psjin230@gmail.com>
2022-02-14 16:24:02 +0900

    09-2

---
+++ Chapter09/src/sec01/ex05/OutterEx.java
@@ -0,0 +1,12 @@
+package sec01.ex05;
+
+public class OutterEx {
+
+	public static void main(String[] args) {
+		Outter outter=new Outter();
+		Outter.Nested nested=outter.new Nested();
+		nested.print();
+
+	}
+
+}
