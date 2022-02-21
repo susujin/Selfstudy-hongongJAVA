93ffccf88a5760af4349769208b4abd661c34ad8
SuJin Park <psjin230@gmail.com>
2022-02-21 19:54:14 +0900

    10-1

---
+++ Chapter10/src/sec01/ArrayIndexOutOfBoundsExceptionEx.java
@@ -0,0 +1,13 @@
+package sec01;
+
+public class ArrayIndexOutOfBoundsExceptionEx {
+
+	public static void main(String[] args) {
+		String data1=args[0];
+		String data2=args[1];
+		
+		System.out.println("args[0]: "+data1);
+		System.out.println("args[0]: "+data2);
+	}
+
+}
